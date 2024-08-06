619ad077e15578659c336de7cbd0f94f0e949c46
Abrshma <118757500+Abrshma@users.noreply.github.com>
2024-08-06 15:41:53 +0700

    Tugas Modul 1-3 Pemrograman Visual

---
+++ belajarjava/src/belajarjava/Belajarjava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package belajarjava;
+
+/**
+ *
+ * @author Erik Pratama
+ */
+public class Belajarjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        percobaan coba1=new percobaan();
+        coba1.setVisible(true);
+    }
+    
+}
